7003d584737b71900dcd941bb79692a08d87a131
PhoenixBUCT <33552455+PhoenixBUCT@users.noreply.github.com>
2017-11-14 11:56:38 +0800

    解决del函数乱码问题

--- student.c
+++ student.c
@@ -166,39 +166,40 @@ void delStudent(Student *ppStu[MAX_STU_NO])
 	int j=0; 
 	long lNo; 
 	char c; 
-	printf("\n ÇëÊäÈëÒªÉ¾³ýµÄÑ§ÉúÑ§ºÅ:"); 
-	scanf("%ld",&lNo); 
-	j=i+1; 
-	for(i=0;i<MAX_STU_NO && ppStu[i]!=NULL;i++)
-	{    
-	    if(ppStu[i]->m_lNo==lNo)
-		{
-		    printf("\n ´æÔÚÒªÉ¾³ýµÄÑ§Éú¼ÇÂ¼");
-			break; 
-		} 
-	} 
-	if((i==MAX_STU_NO)||(ppStu[i]==NULL))
-	{ 
-	    printf("\n ²»´æÔÚÒªÉ¾³ýµÄÑ§Éú¼ÇÂ¼µÄ");
-		return; 
-	}
-	printf("\n********------------********** Õâ ÊÇ Òª É¾ ³ý µÄ ¼Ç Â¼ Âð?**********------------********\n"); 
-	printf("| Ñ§ºÅ | ÐÕÃû | ÐÔ±ð | ¿ÆÄ¿Ò» | ¿ÆÄ¿¶þ | ¿ÆÄ¿Èý | ¿ÆÄ¿ËÄ |\n"); 
-	printf("|-----------|------|------|------|------|------|------|------|--------|------|\n");
-	printf("|%-12s|%-6s|%-6s|%6d|%6d|%6d|%6d|\n",ppStu[i]->m_lNo,ppStu[i]->m_cpName,ppStu[i]->m_cpGender,ppStu[i]->m_lpMajor[0], ppStu[i]->m_lpMajor[1],ppStu[i]->m_lpMajor[2],ppStu[i]->m_lpMajor[3]);
-	printf("\n********------------********** **********------------********\n"); 
-	printf("ÄãÈ·¶¨ÒªÉ¾³ý¸Ã¼ÇÂ¼Âð?Y or N!\n"); 
-	scanf("%s",&c);
-	if((c=='Y')||(c=='N')) 
-	{ 
-		for(j=i+1;j<MAX_STU_NO-i;j++) 
-		{ ppStu[j-1]=ppStu[j]; } 
-		ppStu[j]=NULL;
-        printf("ÕýÔÚÉ¾³ý......\n"); 
-		printf("\n.......ÒÑ¾­É¾³ýÑ§ºÅÎª%ld µÄÑ§Éú¼ÇÂ¼......\n",lNo); } 
+	printf("\n 请输入要删除的学生学号:"); 
+ 	scanf("%ld",&lNo); 
+ 	j=i+1; 
+ 	for(i=0;i<MAX_STU_NO && ppStu[i]!=NULL;i++)
+ 	{    
+ 	    if(ppStu[i]->m_lNo==lNo)
+ 		{
+		    printf("\n 存在要删除的学生记录");
+		    break; 
+ 		} 
+ 	} 
+ 	if((i==MAX_STU_NO)||(ppStu[i]==NULL))
+ 	{ 
+		printf("\n 不存在要删除的学生记录的");
+ 		return; 
+ 	}
+/*输出要删除的学生信息开始*/ 
+	printf("\n********------------********** 这 是 要 删 除 的 记 录 吗?**********------------********\n"); 
+	printf("| 学号 | 姓名 | 性别 | 科目一 | 科目二 | 科目三 | 科目四 |\n"); 
+ 	printf("|-----------|------|------|------|------|------|------|------|--------|------|\n");
+ 	printf("|%-12s|%-6s|%-6s|%6d|%6d|%6d|%6d|\n",ppStu[i]->m_lNo,ppStu[i]->m_cpName,ppStu[i]->m_cpGender,ppStu[i]->m_fpMajor[0], ppStu[i]->m_fpMajor[1],ppStu[i]->m_fpMajor[2],ppStu[i]->m_fpMajor[3]);
+ 	printf("\n********------------********** **********------------********\n"); 
+	printf("你确定要删除该记录吗?Y or N!\n");
+ 	scanf("%s",&c);
+ 	if((c=='Y')||(c=='N')) 
+ 	{ 
+ 		for(j=i+1;j<MAX_STU_NO-i;j++) 
+ 		{ ppStu[j-1]=ppStu[j]; } 
+ 		ppStu[j]=NULL;
+        printf("正在删除......\n"); 
+		printf("\n.......已经删除学号为%ld 的学生记录......\n",lNo); } 
 	else 
-	{ printf("·µ»ØÖ÷²Ëµ¥"); }
- } 
+	{ printf("返回主菜单"); }
+  } 
 
 
 /****************************************
