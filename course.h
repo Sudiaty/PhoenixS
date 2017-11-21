2337b8f9dbab6f45372e5f900d8ef8670fc9b5f1
PhoenixBUCT <sudiaty@163.com>
2017-11-21 18:27:40 +0800

    实现退选课程和打印课表功能

--- course.h
+++ course.h
@@ -31,7 +31,7 @@ static Course pChem[MAX_SUB_NO]={
 
 //声明Course对象具有的方法
 void addCourse(Student *ppStu[MAX_STU_NO]);
-void delCourse(Student *ppStu[MAX_STU_NO]);
-void printCourse(Student *ppStu[MAX_STU_NO]);
+void delCourse(Student *ppStu[MAX_STU_NO],char cpNo[10],long lCourseNo);
+void printCourse(Student *ppStu[MAX_STU_NO],char cpNo[10]);
 
 #endif
